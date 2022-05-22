4d7f6d6191bc0b0813d275f4d6ff75aff141e7f9
wanjianqiang <17688728066@163.com>
2022-05-22 21:22:18 +0800

    fix: 修复打包脚本异常
    fix: 修复打包脚本异常
    
    chore: 修改lerna配置

--- script/release/demo.js
+++
@@ -1,9 +0,0 @@
-const execa = require('execa')
-
-async function main() {
-  const { stdout } = await execa('git', ['branch', '-a'])
-  const hasDevBranch = stdout.split('\n').some(b => b.includes('main'))
-  console.log(hasDevBranch)
-}
-
-main()
