7590a7e72a255760950ddfa60810d25567813dd2
wanjianqiang <17688728066@163.com>
2022-10-16 21:27:31 +0800

    chore: 升级prettier配制
    BREAKING CHANGE: 升级prettier配制

---
+++ packages/prettier/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  printWidth: 80,
+  tableWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  proseWrap: 'preserve',
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf',
+  arrowParens: 'always',
+  embeddedLanguageFormatting: 'auto' // v2.1.0 可用
+  // singleAttributePerLine: 'false' // v2.6.0 可用
+}
