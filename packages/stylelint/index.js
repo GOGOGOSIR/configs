a8e4ea2feed7bf4608c821777cef25117d22c5f4
wanjianqiang <1239531712@qq.com>
2022-11-09 16:10:46 +0800

    feat(stylelint): 对sass语法的校验做补充

--- packages/stylelint/index.js
+++ packages/stylelint/index.js
@@ -6,7 +6,12 @@ module.exports = {
   ],
   rules: {
     // 基础配置
-    'selector-class-pattern': '.', // 校验类名
+    'selector-class-pattern': [
+      '^([#a-z][$#{}a-z0-9]*)((-{1,2}|_{2})[$#{}a-z0-9]+)*$',
+      {
+        message: 'Expected class selector to be kebab-case'
+      }
+    ],
     'string-quotes': 'single',
     'max-line-length': 480,
     'no-duplicate-selectors': null,
@@ -14,11 +19,22 @@ module.exports = {
     'alpha-value-notation': 'number',
     'no-descending-specificity': null, // 允许重复的样式名
 
+    // 兼容 sass
+    'annotation-no-unknown': [
+      true,
+      {
+        ignoreAnnotations: ['default', 'global']
+      }
+    ],
+
     // 兼容 tailwindcss
     'at-rule-no-unknown': [
       true,
       {
         ignoreAtRules: [
+          /** 兼容 sass 的 @forward 语法 */
+          'forward',
+
           'tailwind',
           'apply',
           'layer',
